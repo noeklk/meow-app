bcc227f4d989adbae7bd505b1c9764507996ecc4
Noé ABDEL KALEK <noe.abdelkalek@rentacar.fr>
2026-01-05 02:09:46 +0100

    attempt deploy through remote

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,7 @@
-import {
-    ApplicationConfig,
-    provideAppInitializer,
-    provideBrowserGlobalErrorListeners,
-} from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { initializeApp } from 'firebase/app';
 import { routes } from './app.routes';
-import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideBrowserGlobalErrorListeners(),
-        provideRouter(routes),
-        provideAppInitializer(
-            () => new Promise((r, v) => initializeApp(environment.firebase) && r(true))
-        ),
-    ],
+    providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { MatButton } from '@angular/material/button';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatIcon } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, MatButton, MatToolbar, MatIcon],
+    imports: [RouterOutlet, MatButton, MatToolbar, MatIcon, CommonModule],
     templateUrl: './app.html',
     styleUrls: ['./app.scss'],
 })
